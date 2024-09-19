8b68a0636743873f505c87fd47691a855b3eadf9
zuramai <saugi.dev@gmail.com>
2024-09-19 21:29:01 +0700

    feat: Share your expertise section

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const buttonColors: Record<Color, string> = {
   glass: "bg-glass text-white border-white border hover:bg-glass-bright",
-  primary: "bg-primary",
+  primary: "bg-primary text-white",
   secondary: "bg-secondary",
   outline:
     "border-gray-200 text-gray-200 background-transparent text-white border hover:bg-white/20",
@@ -21,7 +21,7 @@ export default function Button({
   href,
   className,
 }: Props) {
-  const btnClass = `${buttonColors[color]} ${className} focus:ring-4 focus:ring-blue-300 font-semibold inline-block text-sm px-8 py-3 me-1 focus:outline-none transition hover:bg-opacity-90`;
+  const btnClass = `${buttonColors[color]} ${className} text-center focus:ring-4 focus:ring-blue-300 font-semibold inline-block text-sm px-8 py-6 me-1 focus:outline-none transition hover:bg-opacity-90`;
   if (href) {
     return (
       <a href={href} className={btnClass}>
