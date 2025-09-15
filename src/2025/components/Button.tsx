abb5d0b2d6fccb546b443ce6fed4f96a121c12b9
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2025-09-15 11:51:46 +0700

    Init 2025 Design (#191)
    * feat: init hero section idsw 2025
    
    * feat: speakers and about section
    
    * feat: faq section
    
    * feat: highlight section
    
    ---------
    
    Co-authored-by: zuramai <saugi.dev@gmail.com>

---
+++ src/2025/components/Button.tsx
@@ -0,0 +1,37 @@
+import type { Color } from "../@types/components";
+
+interface Props {
+  children: any;
+  color: Color;
+  href?: string;
+  className?: string;
+}
+
+const buttonColors: Record<Color, string> = {
+  glass: "bg-glass text-white border-white border hover:bg-glass-bright",
+  primary: "bg-primary text-white",
+  secondary: "bg-secondary",
+  outline:
+    "border-gray-200 text-gray-200 background-transparent text-white border hover:bg-white/20",
+};
+
+export default function Button({
+  children,
+  color = "primary",
+  href,
+  className,
+}: Props) {
+  const btnClass = `${buttonColors[color]} ${className} text-center focus:ring-4 focus:ring-blue-300 font-semibold inline-block text-sm px-8 py-6 me-1 focus:outline-none transition hover:bg-opacity-90`;
+  if (href) {
+    return (
+      <a href={href} className={btnClass} target="_blank">
+        {children}
+      </a>
+    );
+  }
+  return (
+    <button type="button" className={btnClass}>
+      {children}
+    </button>
+  );
+}
