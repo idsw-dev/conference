aae55354bc87c6f5b856bfd11f2af14c33097618
Bagas Wastu <git@wastu.net>
2025-09-18 09:45:27 +0700

    feat: refactor project structure and update assets for 2025 design
    - Removed outdated files and assets from the 2024 directory.
    - Added new layout and styles for the 2025 design.
    - Introduced new components for agenda, speakers, and social media.
    - Updated image assets for speakers and sponsors.
    - Adjusted configuration to remove unnecessary redirects.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,20 +38,20 @@ export default function Header() {
   return (
     <header>
       <nav
-        className={`fixed left-0 right-0 top-0 z-10 border-gray-200 transition duration-200 ${isScrolled ? "scrolled bg-white shadow-xl" : "bg-transparent"}`}
+        className={`fixed top-0 right-0 left-0 z-10 border-gray-200 transition duration-200 ${isScrolled ? "scrolled bg-white shadow-xl" : "bg-transparent"}`}
       >
         <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between px-10 py-5">
           <a
             href="#"
             className={`flex items-center space-x-3 transition duration-200 rtl:space-x-reverse`}
           >
-            <img src="/2025/nav-logo.png" className="h-8" alt="Flowbite Logo" />
+            <img src="/nav-logo.png" className="h-8" alt="Flowbite Logo" />
           </a>
           <button
             data-collapse-toggle="navbar-default"
             type="button"
             onClick={() => setIsOpen(!isOpen)}
-            className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+            className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 focus:ring-2 focus:ring-gray-200 focus:outline-none md:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-default"
             aria-expanded="false"
           >
@@ -76,13 +76,13 @@ export default function Header() {
             className={`w-full transition duration-200 md:block md:w-auto ${isOpen ? "block" : "hidden"}`}
             id="navbar-default"
           >
-            <ul className="mt-4 flex flex-col rounded-lg border bg-white p-4 font-medium dark:border-gray-700 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-transparent md:p-0 rtl:space-x-reverse">
+            <ul className="mt-4 flex flex-col rounded-lg border bg-white p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-transparent md:p-0 rtl:space-x-reverse dark:border-gray-700">
               {menus.map((menu) => (
                 <li key={menu.name}>
                   <a
                     href={menu.href}
                     onClick={() => setIsOpen(!isOpen)}
-                    className="menu-link block rounded px-3 py-2 text-primary-dark md:bg-transparent md:p-0 lg:text-white"
+                    className="menu-link text-primary-dark block rounded px-3 py-2 md:bg-transparent md:p-0 lg:text-white"
                     aria-current="page"
                   >
                     {menu.name}
