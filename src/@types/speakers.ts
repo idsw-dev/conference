d7c219e22622d922132c46e792cf2e412cb97b81
Bagas Wastu <git@wastu.net>
2025-09-30 15:35:14 +0700

    feat: update speaker data structure and add CFP speaker component
    - Modified Speaker interface to replace 'role' with 'isKeynoteSpeakers' and added optional 'bio' field.
    - Introduced new CFPSpeaker component to display keynote speakers with their details.
    - Updated speakers.json with new entries, including bios and social links for several speakers.
    - Adjusted index.astro to filter and display keynote speakers separately from regular speakers.
    - Added images for new speakers in the data/images/speakers directory.

--- src/@types/speakers.ts
+++ src/@types/speakers.ts
@@ -2,11 +2,12 @@ export interface Speaker {
   name: string;
   title: string;
   imageName: string;
-  role: string;
+  isKeynoteSpeakers: boolean;
   socialUrl: string[];
+  bio?: string;
   talk: {
     title: string;
     description: string;
     topics: string[];
-  };
+  } | null;
 }
