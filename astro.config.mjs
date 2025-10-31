c850235427756281366d3bf2b6930251a8a293a5
Bagas Wastu <git@wastu.net>
2025-10-31 11:56:10 +0700

    feat: update site configuration and improve Open Graph metadata
    - Set site URL dynamically using CF_PAGES_URL or default to "https://www.idsw.dev".
    - Refactor Open Graph and Twitter image URLs to use the dynamic site URL for better SEO and social sharing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.CF_PAGES_URL || "https://www.idsw.dev",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
