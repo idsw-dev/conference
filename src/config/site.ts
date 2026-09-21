e7e6f3e6aa2396cd47507448c8ff0a54addb04f4
Taufiq Septryana <taufiqseptry17@gmail.com>
2026-09-21 07:53:04 +0700

    Add Grafana as a digital partner

--- src/config/site.ts
+++ src/config/site.ts
@@ -86,6 +86,15 @@ export const siteConfig = {
     },
   ],
 
+  digitalPartners: [
+    {
+      name: "Grafana",
+      logo: "/partners/grafana.svg",
+      url: "https://grafana.com",
+      tier: "digital",
+    },
+  ],
+
   communities: [
     {
       name: "Python ID",
